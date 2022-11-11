5127fae83060caa7443fb2d3b36d0f1da6edf99e
Le Mong Thuy <thuyle2000@gmail.com>
2022-11-11 09:24:13 +0700

    Upload Nov 11, 2022

---
+++ d08_function3.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+// chuong trinh c demo cach lap trinh ham co tham so va co gia tri tra ve
+
+long squarer(int n); // khai bao prototype cua ham: long squarer(int)
+
+int main()
+{
+    system("cls");
+
+    // goi ham square de tinh va in ra binh phuong cua 1 so bat ky
+    int x;
+    printf(" nhap 1 so nguyen bat ky: ");
+    scanf("%d", &x);
+    
+    printf("binh phuong [%d] = [%d]\n",x, squarer(x)); 
+    printf("binh phuong [%d] = [%d]\n",x+3, squarer(x+3));   
+    printf("binh phuong [%d] = [%d]\n",12, squarer(12));      
+}
+
+/*
+    Dinh nghia chuong trinh ham tinh binh phuong cua 1 so nguyen bat ky
+    ten ham : squarer
+    gia tri tra ve : so nguyen long
+    tham so : co 1 tham so nguyen n (int n): n la tham so hinh thuc (formal argument)
+*/
+long squarer(int n)
+{
+    long r = n * n;
+    return r;
+}
